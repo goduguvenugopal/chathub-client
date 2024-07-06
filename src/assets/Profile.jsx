5b3dae3f5a3cf23bf659f3e55941632fa3970f8c
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-07-06 21:44:30 +0530

    add

--- src/assets/Profile.jsx
+++ src/assets/Profile.jsx
@@ -1,12 +1,65 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import "../styles/profile.css"
+import { loginTokenContext, profileTokenContext } from '../App';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Profile = () => {
+  const api = import.meta.env.VITE_API_URL;
+  const [loginToken] = useContext(loginTokenContext)
+  const navigate = useNavigate()
+  const [profileToken] = useContext(profileTokenContext)
+  const [proData, setProData] = useState([])
+  const [user, setUser] = useState([])
+  console.log(user);
+
+
+  // fetching user Id 
+  useEffect(() => {
+    const getUserId = async () => {
+
+      const userId = user._id
+      try {
+        const response1 = await axios.get(`${api}/user/get-user`, {
+          headers: {
+            token: loginToken
+          }
+        })
+        if (response1) {
+          setUser(response1.data)
+
+        }
+
+        const response = await axios.get(`${api}/profile/get-profile-byid`, { user: userId })
+        if (response) {
+          setProData(response.data)
+          console.log(response.data);
+
+        }
+
+      } catch (error) {
+        console.log(error);
+
+      }
+    }
+    getUserId()
+
+  }, [])
+
+
+
+  // if token is not available it navigate to login page 
+  useEffect(() => {
+    if (!loginToken || !profileToken) {
+      return navigate("/login")
+    }
+  }, [loginToken, profileToken, navigate])
+
   return (
     <div className='profile-container'>
       <div className='profile-sub-card'>
         <div className='username-top-card'>
-          <h4>venu_gopal_godugu</h4>
+          <h4>venu_gopal</h4>
           <span className="material-symbols-outlined" style={{ cursor: "pointer" }}>
             settings
           </span>
