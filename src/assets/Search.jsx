229482989531a83b09002e1563f3499ba21bd9cd
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-07-09 08:52:40 +0530

    add

--- src/assets/Search.jsx
+++ src/assets/Search.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import "../styles/search.css"
+import axios from 'axios'
+
 
 const Search = () => {
+
   return (
-    <div className='mt-5 p-5'>Search</div>
+    <div className='home-container'>
+      <div className='' style={{ position: "relative" }}>
+        <input type='text' placeholder='Search' className='search-input-box' />
+        <span className="material-symbols-outlined search-icon-in-search">
+          search
+        </span>
+        <button type='submit' className='search-bt'>Search</button>
+      </div>
+    </div>
   )
 }
 
