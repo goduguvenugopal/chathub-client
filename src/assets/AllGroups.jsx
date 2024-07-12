c328c756ae93560f95f8203cc46239d0d0ee73bd
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-07-12 14:39:32 +0530

    add

--- src/assets/AllGroups.jsx
+++ src/assets/AllGroups.jsx
@@ -34,122 +34,111 @@ const AllGroups = () => {
 
             </div>
 
-
-
-
-
             <div className='create-custom-group' onClick={() => setModal(true)}>
                 <span className="material-symbols-outlined">
                     group_add
                 </span>
                 <h5 className='create-custom-group-text'>New Group</h5>
 
-                {/* modal  */}
-                {modal ? <div className='modal-create-custom-group'>
-
-                    <div className="card bg-dark text-white">
-                        <h5 className="card-header">Create New Group</h5><hr className='m-0 ' />
-                        <div className="card-body">
-                            <form className='text-dark d-flex flex-column gap-2'  >
-                                <div className="col">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="Group name"
-                                        aria-label="First name"
-                                    />
-                                </div>
-                                <div className="col">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="1.Profile Id"
-                                        aria-label="First name"
-                                    />
-                                </div>
-                                <div className="col">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="2.Profile Id"
-                                        aria-label="First name"
-                                    />
-                                </div>
-                                <div className="col">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="3.Profile Id"
-                                        aria-label="First name"
-                                    />
-                                </div>
-                                <div className="col">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="4.Profile Id"
-                                        aria-label="First name"
-                                    />
-                                </div>
-                                <div className="col">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="5.Profile Id"
-                                        aria-label="First name"
-                                    />
-                                </div>
-                                <div className="col">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="6.Profile Id"
-                                        aria-label="First name"
-                                    />
-                                </div>
-                                <div className="col">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="7.Profile Id"
-                                        aria-label="First name"
-                                    />
-                                </div>
-                                <div className="col">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="8.Profile Id"
-                                        aria-label="First name"
-                                    />
-                                </div>
-                                <div className="col">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="9.Profile Id"
-                                        aria-label="First name"
-                                    />
-                                </div>
-                                <div className="col">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="10.Profile Id"
-                                        aria-label="First name"
-                                    />
-                                </div>
-
-                                <button className='btn bg-primary text-white fw-bold'>Create Group</button>
-                            </form>
-
-                        </div>
-                    </div>
-
-
-                </div> : ""}
-
+               
             </div>
+  {/* modal  */}
+  {modal ? <div className='modal-create-custom-group'>
+
+<div className="card bg-dark text-white px-3">
+    <div className='d-flex align-items-center justify-content-between'>
+        <h5 className="card-header">Create New Group</h5>
+        <span onClick={()=> setModal(false)} className="material-symbols-outlined" style={{fontSize:"19px", marginTop:"0.4rem", cursor:"pointer"}}  >
+            close
+        </span>
+    </div>
+    <hr className='m-0 ' />
+
+    <div className="card-body">
+        <form className='text-dark d-flex flex-column gap-2'  >
+            <div className="col">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Group name"
+                    aria-label="First name"
+                />
+            </div>
+            <div className="col">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="1.Profile Id"
+                    aria-label="First name"
+                />
+            </div>
+            <div className="col">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="2.Profile Id"
+                    aria-label="First name"
+                />
+            </div>
+            <div className="col">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="3.Profile Id"
+                    aria-label="First name"
+                />
+            </div>
+            <div className="col">
+                <input
+                    type="text"
+
+                    className="form-control"
+                    placeholder="4.Profile Id"
+                    aria-label="First name"
+                />
+            </div>
+            <div className="col">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="5.Profile Id"
+                    aria-label="First name"
+                />
+            </div>
+            <div className="col">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="6.Profile Id"
+                    aria-label="First name"
+                />
+            </div>
+            <div className="col">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="7.Profile Id"
+                    aria-label="First name"
+                />
+            </div>
+            <div className="col">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="8.Profile Id"
+                    aria-label="First name"
+                />
+            </div>
+
+
+            <button className='btn bg-primary text-white fw-bold mt-2'>Create Group</button>
+        </form>
+
+    </div>
+</div>
+
+
+</div> : ""}
 
 
 
